1f4f1e24173085f0d59f038779c1b10ba29f8f97
marino5710 <brianmarin4@gmail.com>
2023-06-10 12:09:26 -0600

    se copio el navbar del repositorio crud_practica 9

---
+++ includes/navbar.php
@@ -0,0 +1,57 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">REGISTRO DE APLICACIONES</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Ventas
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/vistas/ventas/index.php">Ingresar venta</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/vistas/ventas/buscar.php">Buscar venta</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/controladores/ventas/detalle_ventas.php">Detalle de venta</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Productos
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/vistas/productos/index.php">Crear producto</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/vistas/productos/buscar.php">Buscar producto</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Clientes
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/vistas/clientes/index.php">Crear cliente</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/vistas/clientes/buscar.php">Buscar cliente</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
