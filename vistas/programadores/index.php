2a94f4fa3eb49080582483b9f44650bbbbe6ce0a
marino5710 <brianmarin4@gmail.com>
2023-06-10 11:36:35 -0600

    se agregaron los includes al arhivo index

---
+++ vistas/programadores/index.php
@@ -0,0 +1,28 @@
+<?php include_once '../../includes/header.php'?>
+<?php include_once '../../includes/navbar.php'?>}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include_once '../../includes/footer.php'?>
