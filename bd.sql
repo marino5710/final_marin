2fe718cb91e1349574d9e7abbfaca0a1590782ba
marino5710 <brianmarin4@gmail.com>
2023-06-10 11:55:34 -0600

    SE MODIFICO LA TABLA TAREAS

--- bd.sql
+++ bd.sql
@@ -3,20 +3,19 @@ CREATE TABLE aplicaciones (
     aplicacion_id SERIAL PRIMARY KEY,
     apliacion_nombre VARCHAR(70) NOT NULL,
     aplicacion_descripcion VARCHAR (255),
-    aplicacion_fecha_inicio DATETIME,
+    aplicacion_fecha_inicio DATETIME YEAR TO MINUTE,
     aplicacion_situacion char (1) DEFAULT '1'
 );
 
 ---------tabla tareas
-CREATE TABLE tareas (
+CREATE TABLE Tareas (
     tarea_id SERIAL PRIMARY KEY,
     tarea_id_aplicacion INTEGER NOT NULL,
     tarea_descripcion TEXT,
-    tarea_estado VARCHAR(50),
-    tarea_fecha DATETIME,
-    FOREIGN KEY (id_aplicacion) REFERENCES aplicaciones(id)
+    tarea_estado VARCHAR(50) CHECK (tarea_estado IN ('FINALIZADA', 'NO INICIADA')),
+    tarea_fecha DATETIME YEAR TO MINUTE,
+    FOREIGN KEY (tarea_id_aplicacion) REFERENCES Aplicaciones(id)
 );
-
 ------------tabla programadores
 CREATE TABLE programadores (
     programador_id SERIAL PRIMARY KEY,
