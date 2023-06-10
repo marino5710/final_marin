a384836f331f586397f59400181a8668acf81c0a
marino5710 <brianmarin4@gmail.com>
2023-06-10 13:57:04 -0600

    se creo el archivo aplicacion en modelos

---
+++ modelos/Aplicaion.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'Conexion.php';
+
+class Aplicacion extends Conexion{
+    public $aplicacion_id;
+    public $aplicacion_nombre;
+    public $aplicacion_fecha_inicio;
+    public $aplicacion_situacion;
+
+    public function __construct($args = [] )
+    {
+        $this->aplicacion_id = $args['aplicacion_id'] ?? null;
+        $this->aplicacion_nombre = $args['aplicacion_nombre'] ?? '';
+        $this->aplicacion_fecha_inicio = $args['aplicacion_fecha_inicio'] ?? '';
+        $this->aplicacion_situacion = $args['aplicacion_situacion'] ?? '';
+    }
+
+    public function guardar(){
+        $sql = "INSERT INTO aplicaciones (aplicacion_nombre, aplicacion_fecha_inicio, aplicacion_situacion) VALUES ('$this->aplicacion_nombre', '$this->aplicacion_fecha_inicio', '$this->aplicacion_situacion')";
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }
+
+    public function buscar(){
+        $sql = "SELECT * FROM aplicaciones WHERE aplicacion_situacion = 1";
+
+        if($this->aplicacion_nombre != ''){
+            $sql .= " AND aplicacion_nombre LIKE '%$this->aplicacion_nombre%'";
+        }
+
+        if($this->aplicacion_fecha_inicio != ''){
+            $sql .= " AND aplicacion_fecha_inicio = '$this->aplicacion_fecha_inicio'";
+        }
+
+        if($this->aplicacion_id != null){
+            $sql .= " AND aplicacion_id = $this->aplicacion_id";
+        }
+
+        $resultado = self::servir($sql);
+        return $resultado;
+    }
+
+    public function modificar(){
+        $sql = "UPDATE aplicaciones SET aplicacion_nombre = '$this->aplicacion_nombre', aplicacion_fecha_inicio = '$this->aplicacion_fecha_inicio' WHERE aplicacion_id = $this->aplicacion_id";
+        
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }
+
+    public function eliminar(){
+        $sql = "UPDATE aplicaciones SET aplicacion_situacion = 0 WHERE aplicacion_id = $this->aplicacion_id";
+        
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }
+}
+?>
