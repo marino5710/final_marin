ffb8a80c86772dc9faeb6373f750d756805bcc1b
marino5710 <brianmarin4@gmail.com>
2023-06-10 12:06:21 -0600

    se creo el archivo index para guardar a los programadores

--- vistas/programadores/index.php
+++ vistas/programadores/index.php
@@ -1,6 +1,28 @@
 <?php include_once '../../includes/header.php'?>
 <?php include_once '../../includes/navbar.php'?>}
-
+<div class="container">
+        <h1 class="text-center">Formulario de ingreso de clientes</h1>
+        <div class="row justify-content-center">
+            <form action="/final_marin/controladores/programadores/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
+                <div class="row mb-3">
+                    <div class="col">
+                    <label for="programador_nombre">Nombre del Programador</label>
+                        <input type="text" name="programador_nombre" id="progrmador_nombre" class="form-control">
+                    </div>
+                    <div class="row mb-3">
+                    <div class="col">
+                        <label for="programador_apellido">Apellido del Programador</label>
+                        <input type="text" name="programador_apellido" id="programador_apellido" class="form-control">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <button type="submit" class="btn btn-primary w-100">Guardar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 
 
 
