f0c2204671946883765c2e31f1f94d98d454e32a
marino5710 <brianmarin4@gmail.com>
2023-06-10 12:35:05 -0600

    se crel el archivo index formulario de registro de aplicaciones

---
+++ vistas/aplicaciones/index.php
@@ -0,0 +1,33 @@
+<?php include_once '../../includes/header.php'?>
+<?php include_once '../../includes/navbar.php'?>
+    <div class="container">
+        <h1 class="text-center">Formulario de registro de aplicaciones</h1>
+        <div class="row justify-content-center">
+            <form action="/final_marin/controladores/aplicaciones/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="aplicacion_nombre">Nombre de la aplicacion</label>
+                        <input type="text" name="aplicacion_nombre" id="aplicacion_nombre" class="form-control">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="aplicacion_descripcion">descripcion de la aplicacion</label>
+                        <input type="text" name="aplicacion_descripcion" id="aplicacion_descripcion" class="form-control">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="aplicacion_fecha_inicio">fecha de inicio de la aplicacion</label>
+                        <input type="text" name="aplicacion_fecha_inicio" id="aplicacion_fecha_inicio" class="form-control">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <button type="submit" class="btn btn-primary w-100">Guardar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+<?php include_once '../../includes/footer.php'?>
