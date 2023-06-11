40b09fcacf60569f835c424e858eab219ab3f04a
marino5710 <brianmarin4@gmail.com>
2023-06-10 18:04:38 -0600

    se creo el archivo modificar de la carpeta aplicaciones

---
+++ controladores/aplicaciones/modificar.php
@@ -0,0 +1,50 @@
+<?php
+require '../../modelos/Aplicacion.php';
+
+if ($_POST['aplicacion_nombre'] != '' && $_POST['aplicacion_fecha_inicio'] != '' && $_POST['aplicacion_id'] != '') {
+    try {
+        $aplicacion = new Aplicacion($_POST);
+        $resultado = $aplicacion->modificar();
+    } catch (PDOException $e) {
+        $error = $e->getMessage();
+    } catch (Exception $e2) {
+        $error = $e2->getMessage();
+    }
+} else {
+    $error = "Debe llenar todos los datos";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>Resultados</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <?php if($resultado): ?>
+                    <div class="alert alert-success" role="alert">
+                        Modificado exitosamente!
+                    </div>
+                <?php else :?>
+                    <div class="alert alert-danger" role="alert">
+                        Ocurrió un error: <?= $error ?>
+                    </div>
+                <?php endif ?>
+              
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <a href="/final_marin/vistas/aplicaciones/buscar.php?aplicacion_nombre=<?= $_POST['aplicacion_nombre'] ?>" class="btn btn-info">Volver al formulario</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
