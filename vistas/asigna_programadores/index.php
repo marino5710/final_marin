587389fa4b6c0e62e5988c2d14d3418261458f13
marino5710 <brianmarin4@gmail.com>
2023-06-11 13:32:04 -0600

    se crearon los archivos index y modificar asigna programadores

---
+++ vistas/asigna_programadores/index.php
@@ -0,0 +1,59 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once '../../modelos/Aplicacion.php';
+require_once '../../modelos/Programador.php';
+
+try {
+    $aplicacion = new Aplicacion($_GET);
+    $aplicaciones = $aplicacion->buscar();
+
+    $programador = new Programador($_GET);
+    $programadores = $programador->buscar();
+} catch (PDOException $e) {
+    $error = $e->getMessage();
+} catch (Exception $e2) {
+    $error = $e2->getMessage();
+}
+?>
+
+<?php include_once '../../includes/header.php'?>
+<?php include_once '../../includes/navbar.php'?>
+<div class="container">
+    <h1 class="text-center">Formulario de ingreso de programadores y aplicaciones</h1>
+    <div class="row justify-content-center">
+        <form action="/final_marin/controladores/programadores/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
+            <div class="row mb-3">
+                <div class="col">
+                    <label for="asignacion_id_aplicacion">Aplicación</label>
+                    <select name="asignacion_id_aplicacion" id="asignacion_id_aplicacion" class="form-control">
+                        <option value="">SELECCIONE...</option>
+                        <?php foreach ($aplicaciones as $key => $aplicacion) : ?>
+                            <option value="<?= $aplicacion['aplicacion_id'] ?>"><?= $aplicacion['aplicacion_nombre'] ?></option>
+                        <?php endforeach ?>
+                    </select>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col">
+                    <label for="asignacion_id_programador">Programador</label>
+                    <select name="asignacion_id_programador" id="asignacion_id_programador" class="form-control">
+                        <option value="">SELECCIONE...</option>
+                        <?php foreach ($programadores as $key => $programador) : ?>
+                            <option value="<?= $programador['programador_id'] ?>"><?= $programador['programador_nombre'] ?></option>
+                        <?php endforeach ?>
+                    </select>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col">
+                    <button type="submit" class="btn btn-info w-100">Guardar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<?php include_once '../../includes/footer.php'?>
